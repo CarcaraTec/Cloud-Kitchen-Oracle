8dfff75bdd9040da225ad177bf556bf1c2981ae1
Igor Suzuki Kira <89170220+igorsuzuki99@users.noreply.github.com>
2023-08-21 21:40:08 -0300

    initial commit

---
+++ oracleBack/src/main/java/com/fatec/carcara/oracle/oracleBack/OracleBackApplication.java
@@ -0,0 +1,13 @@
+package com.fatec.carcara.oracle.oracleBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OracleBackApplication.class, args);
+	}
+
+}
---
+++ oracleBack/src/test/java/com/fatec/carcara/oracle/oracleBack/OracleBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fatec.carcara.oracle.oracleBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
